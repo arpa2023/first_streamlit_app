0228a786576ad418ae38303dafe969b677d28f66
arpa2023 <125257511+arpa2023@users.noreply.github.com>
2023-02-14 07:01:21 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title("My parents new healthy dinner")
 
-streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 & Blueberry Oatmeal")
-streamlit.text("Kale, Spinach and Rocket Smoothie")
-streamlit.text("Hard-Boiled-Free Range Egg")
+streamlit.header("🥣Breakfast Favorites")
+streamlit.text("🥗 Omega 3 & Blueberry Oatmeal")
+streamlit.text("🐔Kale, Spinach and Rocket Smoothie")
+streamlit.text("🥑🍞Hard-Boiled-Free Range Egg")
 
